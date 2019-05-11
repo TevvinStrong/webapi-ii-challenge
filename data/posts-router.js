ddfc54d324659c37e141ca4e07307d7f15f2de8c
Tevvin <Tevvin7395@icloud.com>
2019-05-11 12:37:17 -0400

    (Get) Returns the post object with the specified id

--- data/posts-router.js
+++ data/posts-router.js
@@ -16,17 +16,29 @@ router.get('/', async (req, res) => {
     try {
         const posts = await Posts.find(req.query);
         res.status(200).json(posts);
-        console.log(posts);
+        //console.log(posts);
     } catch (error) {
         res.status(500).json({
-            error: "Error retreving the posts",
+            error: "The posts information could not be retrieved.",
         });
     }
 });
 
 // Get
-router.get('', (req, res) => {
+router.get('/api/posts/:id', async (req, res) => {
+    const { id } = req.params;
 
+    try {
+        const postsId = await Posts.findById(id);
+
+        if (postsId.length) {
+            res.json(postsId);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    } catch (error) {
+        res.status(500).json({ err: "Try again." });
+    }
 });
 
 // Delete
