c5b71f705a32aefcc38412751e4bd8ed72bf5158
Tevvin <Tevvin7395@icloud.com>
2019-05-11 13:10:36 -0400

    (Delete) Removes the post with the specified id and returns the deleted post object.

--- data/posts-router.js
+++ data/posts-router.js
@@ -47,8 +47,17 @@ router.get('/api/posts/:id', async (req, res) => {
 });
 
 // Delete
-router.delete('', (req, res) => {
-
+router.delete('/api/posts/:id', async (req, res) => {
+    try {
+        const deletePosts = await Posts.remove(req.params.id);
+        if (deletePosts > 0) {
+            res.status(200).json({ message: "The selected posts has been destroyed." });
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "The post could not be removed" });
+    }
 });
 
 // Put
