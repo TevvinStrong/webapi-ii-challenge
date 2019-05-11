2c3f33286997c41ecc8047c6839c52d8dda51fc6
Tevvin <Tevvin7395@icloud.com>
2019-05-10 20:20:35 -0400

    (Get request)- Returns an array of all the posts objects contained in the database

--- data/posts-router.js
+++ data/posts-router.js
@@ -1,11 +1,43 @@
 const express = require('express');
 
-const db = require('./db.js');
+const Posts = require('./db.js');
 
 const router = express.Router();
 
-// Routes
+//*** Routes ***\\
 
+// Post
+router.post('', (req, res) => {
+
+});
+
+// Get
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find(req.query);
+        res.status(200).json(posts);
+        console.log(posts);
+    } catch (error) {
+        res.status(500).json({
+            error: "Error retreving the posts",
+        });
+    }
+});
+
+// Get
+router.get('', (req, res) => {
+
+});
+
+// Delete
+router.delete('', (req, res) => {
+
+});
+
+// Put
+router.put('', (req, res) => {
+
+});
 
 
 module.exports = router;
